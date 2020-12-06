3ef8e0589e56a52fb769850bbe5ec5d2b15e0692
Jeremy Langevin <langevinj832@gmail.com>
2020-12-06 07:32:36 -0500

    Fixed JobCard to allow for unapplying and applying for multiple jobs at a time

--- src/JobCard.js
+++ src/JobCard.js
@@ -2,29 +2,24 @@ import React from 'react'
 import JoblyApi from './JoblyApi'
 import './JobCard.css'
 import { useLocalStorage } from './hooks'
-import { useHistory } from 'react-router-dom'
 import { renderJobButton } from './helpers'
 
-function JobCard({ job }){
+function JobCard({ job, toggleJob  }){
     const [user, setUser] = useLocalStorage("user")
-    const history = useHistory();
 
     const applyForJob = async (evt) => {
         evt.preventDefault()
         let res = await JoblyApi.applyToJob(user.user.username, job.id)
         setUser(user => ({user: {...user.user, applications: [...user.user.applications, job.id]}}))
+        toggleJob()
     }
 
     const unApply = async (evt) => {
         evt.preventDefault();
         let res = await JoblyApi.unapplyToJob(user.user.username, job.id)
-        let oldApplications = [...user.user.applications]
-        const idx = oldApplications.indexOf(job.id)
-        if(idx > -1){
-            oldApplications.splice(idx, 1);
-        }
-        setUser(user => ({ user: { ...user.user, applications: [...oldApplications ]}}))
-        history.push("/jobs")
+        let removedArray = user.user.applications.filter(j => j !== job.id)
+        setUser(user => ({ user: {...user.user, applications: [...removedArray]}}))
+        toggleJob()
     }
 
     return(
