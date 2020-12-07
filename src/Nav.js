f78a9b2ed08251f46833de243693feb02304264b
Jeremy Langevin <langevinj832@gmail.com>
2020-12-07 12:57:07 -0500

    Attempting to make nav-bar toggler work

--- src/Nav.js
+++ src/Nav.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import $ from 'jquery'
 import { NavLink } from 'react-router-dom'
 import './Nav.css'
 
@@ -11,23 +12,30 @@ function Nav({user}){
     //navbar view if a user is logged in
     const loggedInView = (
         <nav className="navbar navbar-expand-lg bg-white">
-            <NavLink exact to="/" className="Navbar-link navbar-brand">Jobly</NavLink>
+            <div className="container-fluid">
+                <NavLink exact to="/" className="Navbar-link navbar-brand">Jobly</NavLink>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target={$('#navbarNavDropdown')} aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul className="navbar-nav ml-auto">
                     <li className="nav-item active">
                         <NavLink exact to="/companies" className="nav-link" activeStyle={ ACTIVE_STYLES }>Companies</NavLink>
                     </li>
-                    <li className="nav-item active">
+                    <li className="nav-item">
                         <NavLink exact to="/jobs" className="nav-link" activeStyle={ACTIVE_STYLES}>Jobs</NavLink>
                     </li>
-                    <li className="nav-item active">
-                        <NavLink exact to="/profile" className="nav-link" activeStyle={ACTIVE_STYLES}>Profile</NavLink>
+                    <li className="nav-item">
+                        <NavLink exact to="/profile" className="nav-link navbar-text" activeStyle={ACTIVE_STYLES}>Profile</NavLink>
                     </li>
-                    <li className="nav-item active">
+                    <li className="nav-item">
                         <NavLink exact to="/logout" className="nav-link" activeStyle={ACTIVE_STYLES}>Logout</NavLink>
                     </li>
                 </ul>
+                </div>
+                </div>
         </nav>
-    )
+)
     
     //navbar view if a user is logged out
     const loggedOutView = (
