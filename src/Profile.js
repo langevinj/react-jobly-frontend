7f3a1e727dbb415371b12d161b45310a0cf5f5c2
Jeremy Langevin <langevinj832@gmail.com>
2020-12-07 11:40:05 -0500

    edited profile page style

--- src/Profile.js
+++ src/Profile.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import EditProfile from './EditProfile'
 import { useLocalStorage } from './hooks'
 import JoblyApi from './JoblyApi'
+import './Profile.css'
 
 function Profile() {
     const [user, setUser] = useLocalStorage("user")
@@ -26,11 +27,29 @@ function Profile() {
     }
 
     const staticView = u ? (
-        <div className="Profile-card">
-            <h2>{u.username}</h2>
-            <h3>{u.firstName}</h3>
-            <h3>{u.lastName}</h3>
-            <h3>{u.email}</h3>
+        // <div className="Profile-card">
+        //     <h2>Username: {u.username}</h2>
+        //     <h3>Name: {u.firstName} {u.lastName}</h3>
+        //     <h3>{u.email}</h3>
+        //     
+        // </div>
+        <div className="profile-info">
+            <table className="table table-dark">
+                <tbody>
+                    <tr>
+                        <th scope="row">Username:</th>
+                        <td>{u.username}</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Name:</th>
+                        <td>{u.firstName} {u.lastName}</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Email:</th>
+                        <td>{u.email}</td>
+                    </tr>
+                </tbody>
+            </table>
             <button onClick={toggleView}>Edit Profile</button>
         </div>
     ) : null
