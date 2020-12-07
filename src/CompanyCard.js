2cac304ab4baae4debb6ba9c009b5090123e54f5
Jeremy Langevin <langevinj832@gmail.com>
2020-12-07 11:08:57 -0500

    Adjusted default image for companies

--- src/CompanyCard.js
+++ src/CompanyCard.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import './CompanyCard.css'
 import { Link } from 'react-router-dom'
+import defaultLogo from './defaultLogo.png'
 
 function CompanyCard({ company }) {
-    const logoSource = company.logo_url ? company.logo_url : './defaultLogo.png'
+    // can use below code if accurate company images are present in the database
+    // const logoSource = company.logoUrl ? company.logo_url : defaultLogo
 
     return(
         <div className="CompanyCard">
                 <h6 className="card-title">
                     <span className="title">{company.name}</span>
-                    <img className="logo" src={logoSource}></img>
+                    <img className="logo" src={defaultLogo} alt="the company logo"></img>
                 </h6>
                 <p className="description">{company.description}</p>
         </div>
