c56382c1d3a4aaa3ee8e4caa201fc23730e51c59
Jeremy Langevin <langevinj832@gmail.com>
2020-12-07 13:07:50 -0500

    Signup form stylized

--- src/SignupForm.js
+++ src/SignupForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import JoblyApi from './JoblyApi'
 import { useHistory } from 'react-router-dom'
+import './SignupForm.css'
 
 function SignupForm({logIn}) {
     const history = useHistory();
@@ -40,17 +41,27 @@ function SignupForm({logIn}) {
 
     return (
         <form className="signup-form" onSubmit={handleSubmit}>
-            <label htmlFor="username">Username</label>
-            <input name="username" value={formData.username} id="username" onChange={handleChange} type="text"></input>
-            <label htmlFor="password">Password</label>
-            <input name="password" value={formData.password} id="password" onChange={handleChange} type="password"></input>
-            <label htmlFor="firstName">First Name</label>
-            <input name="firstName" value={formData.firstName} id="firstName" onChange={handleChange} type="text"></input>
-            <label htmlFor="lastName">Last Name</label>
-            <input name="lastName" value={formData.lastName} id="lastName" onChange={handleChange} type="text"></input>
-            <label htmlFor="email">Email</label>
-            <input name="email" value={formData.email} id="email" onChange={handleChange} type="text"></input>
-            <button className="submitButton">Submit</button>
+            <div className="form-group">
+                <label htmlFor="username">Username:</label>
+                <input name="username" value={formData.username} id="username" onChange={handleChange} type="text" className="form-control"></input>
+            </div>
+            <div className="form-group">
+                <label htmlFor="password">Password:</label>
+                <input name="password" value={formData.password} id="password" onChange={handleChange} type="password" className="form-control"></input>
+            </div>
+            <div className="form-group">
+                <label htmlFor="firstName">First Name:</label>
+                <input name="firstName" value={formData.firstName} id="firstName" onChange={handleChange} type="text" className="form-control"></input>
+            </div>
+            <div className="form-group">
+                <label htmlFor="lastName">Last Name:</label>
+                <input name="lastName" value={formData.lastName} id="lastName" onChange={handleChange} type="text" className="form-control"></input>
+            </div>
+            <div className="form-group">
+                <label htmlFor="email">Email:</label>
+                <input name="email" value={formData.email} id="email" onChange={handleChange} type="text" className="form-control"></input>
+            </div>
+            <button className="submitButton btn-primary rounded">Submit</button>
         </form>
     )
 }
