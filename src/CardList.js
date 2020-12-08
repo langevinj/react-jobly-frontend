cf1f93a9338e528fe79c0ab0fa27f5978157c617
Jeremy Langevin <langevinj832@gmail.com>
2020-12-08 11:56:07 -0500

    reworked CardList

--- src/CardList.js
+++ src/CardList.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import Card from './Card'
-import { v4 as uuid } from 'uuid'
 import './CardList.css'
 
-function CardList({ title, items, toggleJob, pageNum }) {
+function CardList({ title, items, apply = () => null, pageNum }) {
     return (
         <div className="CardList">
-            {items[pageNum].map(item => (item ? <Card key={uuid()} title={title} item={item} toggleJob={toggleJob}/> : null))}
+            {items[pageNum].map(item, idx => (item ? <Card key={idx} id={idx} title={title} item={item} apply={apply}/> : null))}
         </div>
     )
 }
