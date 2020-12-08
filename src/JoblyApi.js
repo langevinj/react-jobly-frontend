62245bdd478d38aff356ceff0cac174ead6f769b
Jeremy Langevin <langevinj832@gmail.com>
2020-12-08 14:09:51 -0500

    Profile form moved to Profile component

--- src/JoblyApi.js
+++ src/JoblyApi.js
@@ -4,35 +4,54 @@ import { TOKEN_KEY } from './App.js'
 const BASE_URL = process.env.BASE_URL || "http://localhost:3001";
 
 class JoblyApi {
-    static async request(endpoint, params = {}, verb = "get") {
+    static token;
 
-        //get the token from localStorage
-        let _token = localStorage.getItem(TOKEN_KEY)
+    static async request(endpoint, data = {}, method = "get") {
+        console.debug("API Call:", endpoint, data, method);
 
-        console.debug("API Call:", endpoint, params, verb);
-
-        let query;
-
-        //set the correct data for the query to the API based on the verb
-        if (verb === "get") {
-            query = axios.get(
-                `${BASE_URL}/${endpoint}`, {headers: {'authorization': _token}}, {params: { _token, ...params } });
-        } else if (verb === "post") {
-            query = axios.post(
-                `${BASE_URL}/${endpoint}`, { headers: { 'authorization': _token } }, { _token, ...params });
-        } else if (verb === "patch") {
-            query = axios.patch(
-                `${BASE_URL}/${endpoint}`, { headers: { 'authorization': _token } }, { _token, ...params });
-        }
-
+        const url = `${BASE_URL}/${endpoint}`;
+        const headers = { Authorization: `Bearer ${JoblyApi.token}` };
+        const params = (method === "get")
+            ? data
+            : {};
+        
         try {
-            return (await query).data;
+            return (await axios({ url, method, data, params, headers })).data;
         } catch (err) {
             console.error("API Error:", err.response);
-            let message = err.response.data.message;
-            throw Array.isArray(message) ? message : [message];
+            let message = err.response.data.error.message;
+            throw Array.isArray(message) ? message: [message]
         }
     }
+    // static async request(endpoint, params = {}, verb = "get") {
+
+    //     //get the token from localStorage
+    //     let _token = localStorage.getItem(TOKEN_KEY)
+
+    //     console.debug("API Call:", endpoint, params, verb);
+
+    //     let query;
+    //     const headers = { Authorization: `Bearer ${_token}` };
+    //     //set the correct data for the query to the API based on the verb
+    //     if (verb === "get") {
+    //         query = axios.get(
+    //             `${BASE_URL}/${endpoint}`, {params: { _token, ...params } }, headers);
+    //     } else if (verb === "post") {
+    //         query = axios.post(
+    //             `${BASE_URL}/${endpoint}`, { _token, ...params }, headers);
+    //     } else if (verb === "patch") {
+    //         query = axios.patch(
+    //             `${BASE_URL}/${endpoint}`, { _token, ...params }, headers);
+    //     }
+
+    //     try {
+    //         return (await query).data;
+    //     } catch (err) {
+    //         console.error("API Error:", err.response);
+    //         let message = err.response.data.message;
+    //         throw Array.isArray(message) ? message : [message];
+    //     }
+    // }
     
     //get a single company by its handle
     static async getCompany(handle) {
--- src/Profile.js
+++ src/Profile.js
@@ -1,55 +1,93 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useContext, useRef } from 'react'
 import EditProfile from './EditProfile'
-import useLocalStorage from './hooks'
 import JoblyApi from './JoblyApi'
 import './Profile.css'
+import UserContext from './UserContext'
+import './Forms.css'
 
 function Profile() {
-    const [user, setUser] = useLocalStorage("user")
-    const [editView, setEditView] = useState(false)
-    const [u, setU] = useState(user.user)
+    const { currUser, setCurrUser } = useContext(UserContext)
+    // const [user, setUser] = useLocalStorage("user")
+    // const [editView, setEditView] = useState(false)
+    // const [u, setU] = useState(user.user)
 
-    const toggleView = () => {
-        setEditView(!editView)
-    }
+    const [formData, setFormData] = useState({
+        firstName: currUser.firstName || "",
+        lastName: currUser.lastName || "",
+        email: currUser.email || "",
+        username: currUser.username,
+        password: "",
+        errors: [],
+        saveConfirmed: false
+    });
 
-    useEffect(() => {
-        function loadU() {
-            setU(user.user)
+    // const toggleView = () => {
+    //     setEditView(!editView)
+    // }
+    
+    //submit changes and update via the backend
+    const handleSubmit = async (evt) => {
+        evt.preventDefault();
+
+        try {
+            let profileData = {
+                firstName: formData.firstName || undefined,
+                lastName: formData.lastName || undefined,
+                email: formData.email || undefined,
+                password: formData.password
+            };
+
+            let username = formData.username;
+            let updatedUser = await JoblyApi.saveProfile(username, profileData);
+            console.log("UPDATED USER", updatedUser)
+            setFormData(f => ({
+                ...f,
+                errors: [],
+                password: ""
+            }));
+            setCurrUser(updatedUser);
+        } catch (errors) {
+            setFormData(f => ({ ...f, errors }));
         }
-        loadU()
-    }, [editView, user.user])
-
-    const updateUser = async (updatedInfo) => {
-        let res = await JoblyApi.updateUser(user.user.username, updatedInfo)
-        setUser({ user: { ...res.user, applications: [...user.user.applications] } })
-        toggleView()
     }
 
-    const staticView = u ? (
-        <div className="profile-info">
-            <table className="table table-dark">
-                <tbody>
-                    <tr>
-                        <th scope="row">Username:</th>
-                        <td>{u.username}</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">Name:</th>
-                        <td>{u.firstName} {u.lastName}</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">Email:</th>
-                        <td>{u.email}</td>
-                    </tr>
-                </tbody>
-            </table>
-            <button onClick={toggleView}>Edit Profile</button>
-        </div>
-    ) : null
+    function handleChange(evt) {
+        const {name, value} = evt.target;
+        setFormData(f => ({
+            ...f,
+            [name]: value,
+            errors: []
+        }));
+    }
+
+    // const staticView = u ? (
+     
+    // ) : null
 
     return (
-        <>{editView ? <EditProfile toggleView={toggleView} u={u} updateUser={updateUser} /> : staticView}</>
+        <div className="EditProfile-card form-container">
+            <h2>{formData.username}</h2>
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label htmlFor="firstName">First Name:</label>
+                    <input type="text" name="firstName" id="firstName" value={formData.firstName} onChange={handleChange} className="form-control"></input>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="email">Last Name:</label>
+                    <input type="text" name="lastName" id="lastName" value={formData.lastName} onChange={handleChange} className="form-control"></input>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="email">Email:</label>
+                    <input type="text" name="email" id="email" value={formData.email} onChange={handleChange} className="form-control"></input>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Confirm password to make changes:</label>
+                    <input type="password" name="password" id="password" value={formData.password} onChange={handleChange} className="form-control"></input>
+                </div>
+                <button className="btn-primary rounded" onClick={handleSubmit}>Save Changes</button>
+            </form>
+        </div>
+        // <>{editView ? <EditProfile toggleView={toggleView} u={u} updateUser={updateUser} /> : staticView}</>
     )
 }
 
