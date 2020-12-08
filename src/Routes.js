3158fdcbea5b5b154c100bf9a8dad274dca858a2
Jeremy Langevin <langevinj832@gmail.com>
2020-12-08 11:06:26 -0500

    Updated routes

--- src/Routes.js
+++ src/Routes.js
@@ -11,11 +11,11 @@ import UserContext from './UserContext'
 
 function Routes({setToken}){
     const { currUser } = useContext(UserContext);
-    
+
     const loggedOutRoutes = (
         <Switch>
-            <Route exact path="/login"><Login logIn={logIn} /></Route>
-            <Route exact path="/"><Home user={user} /></Route>
+            <Route exact path="/login"><Login setToken={setToken} /></Route>
+            <Route exact path="/"><Home /></Route>
             <Redirect to="/"></Redirect>
         </Switch>
     )
@@ -23,12 +23,12 @@ function Routes({setToken}){
     const loggedInRoutes = (
         <Switch>
             <Route exact path="/companies"><Companies /></Route>
-            <Route exact path="/companies/:handle"><Company toggleJob={toggleJob}/></Route>
-            <Route exact path="/jobs"><JobsList toggleJob={toggleJob} jobAdded={jobAdded} /></Route>
-            <Route exact path="/login"><Login logIn={logIn} /></Route>
-            <Route exact path="/logout"><Logout logOut={logOut} /></Route>
+            <Route exact path="/companies/:handle"><Company /></Route>
+            <Route exact path="/jobs"><JobsList /></Route>
+            {/* <Route exact path="/login"><Login logIn={logIn} /></Route> */}
+            {/* <Route exact path="/logout"><Logout logOut={logOut} /></Route> */}
             <Route exact path="/profile"><Profile /></Route>
-            <Route exact path="/"><Home user={user} /></Route>
+            <Route exact path="/"><Home /></Route>
             <Redirect to="/"></Redirect>
         </Switch>
     )
