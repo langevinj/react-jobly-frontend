66732ad9b4a4e720ce8f6e92f9942fe85ec3bf5c
Jeremy Langevin <langevinj832@gmail.com>
2020-12-09 13:13:34 -0500

    Rewrote logic in CardList to avoid bug

--- src/CardList.js
+++ src/CardList.js
@@ -2,11 +2,12 @@ import React from 'react'
 import Card from './Card'
 import './CardList.css'
 
-function CardList({ title, items = [], apply = () => null, unapply = () => null, pageNum }) {
+function CardList({ title, items = [], apply = () => null, unapply = () => null, pageNum = 0}) {
     //if absolutely no results are found, tell the user, otherwise list as many items as the page provides
+    const pageItems = items[0] && Array.isArray(items[0]) ? items[pageNum] : items 
     return items.length ? (
         <div className="CardList">
-            {items[pageNum].map((item, idx) => item ? (<Card key={idx} id={item.id} title={title} item={item} apply={apply} unapply={unapply}/>) : null)}
+            {pageItems.map((item, idx) => item ? (<Card key={idx} id={item.id} title={title} item={item} apply={apply} unapply={unapply}/>) : null)}
         </div>
     ) : (
         <p>Sorry, no results found!</p>
--- src/CardList.test.js
+++ src/CardList.test.js
@@ -7,4 +7,10 @@ import CardList from './CardList'
 
 it("renders without crashing", function() {
     render(<CardList />);
+});
+
+it("matches the snapshot", function() {
+    const { asFragment } = render(<CardList items={["testcard1", "testcard2"]}/>)
+
+    expect(asFragment()).toMatchSnapshot();
 })
