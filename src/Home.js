11d2fee7af810b1c8e4e13718496f554ec7c71ac
Jeremy Langevin <langevinj832@gmail.com>
2020-12-09 10:38:24 -0500

    Cleaning up, refactoring, and documentation of Home component

--- src/Home.js
+++ src/Home.js
@@ -1,49 +1,36 @@
 import React, { useContext } from 'react'
-import './Home.css'
 import { Link } from 'react-router-dom'
-import UserContext from './UserContext'
 
+import UserContext from './UserContext'
+import './Home.css'
+
+//Component for rendering the homepage
 function Home() {
+    //get the current user
     const { currUser } = useContext(UserContext);
 
-    const loggedOutView = (
-        <div className="container-fluid bg-light home">
-            <div className="row align-items-start">
-                <div className="col-12"></div>
-            </div>
-            <div className="row prompt-row align-items-center">
-                <div className="col-4"></div>
-                <div className="col-4 login-box rounded">
-                    <h2>Jobly</h2>
-                    <p>Start the hunt for your next position with us.</p>
-                    <Link to="/login"><button type="button" className="btn btn-primary">Login</button></Link>
-                </div>
-                <div className="col-4"></div>
-            </div>
-        </div>
-    )
+    //prompt box if user logged out
+    const loggedOutView = <><p>Start the hunt for your next position with us.</p>
+        <Link to="/login"><button type="button" className="btn btn-primary">Login</button></Link></>
 
-    const loggedInView = (
-        <div className="container-fluid bg-light home">
-            <div className="row align-items-start">
-                <div className="col-12"></div>
-            </div>
-            <div className="row prompt-row align-items-center">
-                <div className="col-4"></div>
-                <div className="col-4 login-box rounded">
-                    <h2>Jobly</h2>
-                    <p>Explore all the job postings we have to offer.</p>
-                    <h4>Welcome Back!</h4>
-                </div>
-                <div className="col-4"></div>
-            </div>
-        </div>
-    )
+    //welcome box if user logged in
+    const loggedInView = <><p>Explore all the job postings we have to offer.</p>
+        <h4>Welcome Back!</h4></>
 
     return (
-        <>
-            {!currUser ? loggedOutView : loggedInView}
-        </>
+        <div className="container-fluid bg-light home">
+            <div className="row align-items-start">
+                <div className="col-12"></div>
+            </div>
+            <div className="row prompt-row align-items-center">
+                <div className="col-4"></div>
+                <div className="col-4 login-box rounded">
+                    <h2>Jobly</h2>
+                    {currUser ? loggedInView : loggedOutView}
+                </div>
+                <div className="col-4"></div>
+            </div>
+        </div>
     )
 }
 
