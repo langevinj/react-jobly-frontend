a8393fb1c18bfdd8912c344baae0f7b399d0db8a
Jeremy Langevin <langevinj832@gmail.com>
2020-12-09 13:18:28 -0500

    Tests written for Companies component

--- src/CardList.test.js
+++ src/CardList.test.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom'
-import { MemoryRouter } from 'react-router-dom'
-import { jobPages } from './_testCommon'
 import CardList from './CardList'
 
 it("renders without crashing", function() {
--- src/Companies.test.js
+++ src/Companies.test.js
@@ -1,26 +1,13 @@
 import React from 'react'
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom'
-import { MemoryRouter } from 'react-router-dom'
 import Companies from './Companies'
 
-//server must be running to make correct calls
-
-//mock inner components
-jest.mock('./Search', () => () => 'Search')
-jest.mock('./CardList', () => () => 'CardList')
-jest.mock('./PageButtons', () => () => 'PageButtons')
-
-it("renders without crashing", function () {
-    render(<MemoryRouter>
-        <Companies />
-    </MemoryRouter>)
+it("renders without crashing", function() {
+    render(<Companies />);
 });
 
-it("matches the snapshot", function () {
-    const { asFragment } = render(<MemoryRouter>
-        <Companies />
-    </MemoryRouter>)
-
-    expect(asFragment).toMatchSnapshot();
+it("matches snapshot", function() {
+    const { asFragment } = render(<Companies />)
+    expect(asFragment()).toMatchSnapshot();
 });
