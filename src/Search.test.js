1f296f41910c4fc7d1ba466fee5d0efaf7f755d2
Jeremy Langevin <langevinj832@gmail.com>
2020-12-09 14:00:16 -0500

    Tests written for Search component

---
+++ src/Search.test.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import Search from './Search'
+
+it("renders without crashing", function() {
+    render(<Search />);
+});
+
+it("matches snapshot", function() {
+    const { asFragment } = render(<Search />);
+    expect(asFragment()).toMatchSnapshot();
+});
+
