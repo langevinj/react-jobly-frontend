e443bd4d2add41c123ce8e032e1c876e671ecebb
Jeremy Langevin <langevinj832@gmail.com>
2020-12-09 12:42:30 -0500

    Tests written for App component

--- src/App.test.js
+++ src/App.test.js
@@ -1,21 +1,7 @@
 import React from 'react'
-import { render } from '@testing-library/react';
-import '@testing-library/jest-dom'
-import { MemoryRouter } from 'react-router-dom'
-import axiosMock from 'axios'
-import App from './App';
+import { render } from '@testing-library/react'
+import App from './App'
 
-it('renders without crashing', function () {
-  render(<MemoryRouter>
-    <App />
-  </MemoryRouter>);
-});
-
-it('matches the snapshot', function () {
-  const { asFragment } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>);
-
-    expect(asFragment).toMatchSnapshot();
+it('renders without crashing', function() {
+  render(<App />);
 });
