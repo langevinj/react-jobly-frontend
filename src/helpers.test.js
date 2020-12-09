d990f46da51528961ebf0e4ed6e18b8ebc0397cd
Jeremy Langevin <langevinj832@gmail.com>
2020-12-09 13:29:40 -0500

    Unecessary tests removed from helpers tests

--- src/helpers.test.js
+++ src/helpers.test.js
@@ -1,21 +1,4 @@
-import { renderJobButton, paginateData } from './helpers'
-
-/** renderJobButton */
-const applyForJob = () => {return ""}
-const unApply = () => {return ""}
-const applications = [1, 2, 3]
-
-describe("renderJobButton", function () {
-    test("returns an applied for job button correctly", function () {
-        let res = renderJobButton(applications, 1, applyForJob, unApply)
-        expect(res).toEqual(<button type="button" data-toggle="button" className="Apply btn-secondary rounded" onClick={unApply}>Applied</button>)
-    });
-
-    test("returns a not yet applied for job button correctly", function () {
-        let res = renderJobButton(applications, 4, applyForJob, unApply)
-        expect(res).toEqual(<button type="button" data-toggle="button" className="Apply btn-primary rounded" onClick={applyForJob}>Apply</button>);
-    });
-});
+import { paginateData } from './helpers'
 
 const arrayOfData = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 const arrayOfData2 = [1, 2, 3, 4, 5, 6, 7, 8]
